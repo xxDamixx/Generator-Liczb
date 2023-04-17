c3a3809ede98f33ad86713849f5cd5a96cfd7eb2
Damian P <117529645+xxDamixx@users.noreply.github.com>
2023-04-17 10:28:33 +0200

    Ver. 1.0
    Gotowy

---
+++ programdoliczb.py
@@ -0,0 +1,9 @@
+import random
+
+tolerance = 0.1  # 10% tolerance
+
+while True:
+    for i in range(4):
+        random_number = random.uniform(500 - (500 * tolerance), 500 + (500 * tolerance))
+        print(random_number)
+    input("wciśnij dowolny przycisk żeby wygenerować nowe liczby")
